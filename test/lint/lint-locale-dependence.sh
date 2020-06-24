9d6207eb46e1001033f148e864800f8d47b321f5
Malcolm MacLeod <malcolm@gulden.com>
2020-06-24 11:32:32 +0200

    Revert "CORE: Remove all Qt code/dependencies"
    This reverts commit bcffdc58cdb6d03b6048b74016d7068702484cfa.

--- test/lint/lint-locale-dependence.sh
+++ test/lint/lint-locale-dependence.sh
@@ -9,6 +9,7 @@ KNOWN_VIOLATIONS=(
     "src/httprpc.cpp.*trim"
     "src/init.cpp:.*atoi"
     "src/init.cpp:.*fprintf"
+    "src/qt/rpcconsole.cpp:.*atoi"
     "src/rest.cpp:.*strtol"
     "src/test/dbwrapper_tests.cpp:.*snprintf"
     "src/torcontrol.cpp:.*atoi"
