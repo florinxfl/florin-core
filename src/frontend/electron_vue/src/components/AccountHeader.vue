da608cc7ce893c41c04fd31bf93fd638d6d888a6
LukeV111 <luke@purist.industries>
2022-07-11 12:20:46 +0200

    Allows Deletion of Account

--- src/frontend/electron_vue/src/components/AccountHeader.vue
+++ src/frontend/electron_vue/src/components/AccountHeader.vue
@@ -1,40 +1,49 @@
 <template>
-  <div class="account-header">
-    <div v-if="!editMode" class="flex-row">
-      <div v-if="isSingleAccount" class="flex-row flex-1">
-        <div class="logo" />
-        <div class="balance-row flex-1">
-          <span>{{ balanceForDisplay }}</span>
-          <span>{{ totalBalanceFiat }}</span>
-        </div>
-      </div>
-      <div v-else class="left-colum" @click="editName">
-        <account-tooltip type="Account" :account="account">
-          <div class="flex-row flex-1">
-            <div class="accountname ellipsis">{{ name }}</div>
-            <fa-icon class="pen" :icon="['fal', 'fa-pen']" />
-          </div>
-
-          <div class="balance-row">
+  <div>
+    <confirm-dialog v-model="modal" />
+    <div class="account-header">
+      <div v-if="!editMode" class="flex-row">
+        <div v-if="isSingleAccount" class="flex-row flex-1">
+          <div class="logo" />
+          <div class="balance-row flex-1">
             <span>{{ balanceForDisplay }}</span>
             <span>{{ totalBalanceFiat }}</span>
           </div>
-        </account-tooltip>
-      </div>
-      <div v-if="showBuySellButtons">
-        <button outlined class="small" @click="buyCoins" :disabled="buyDisabled">{{ $t("buttons.buy") }}</button>
-        <button outlined class="small" @click="sellCoins" :disabled="sellDisabled">{{ $t("buttons.sell") }}</button>
-      </div>
-      <div v-if="isSingleAccount" class="flex-row icon-buttons">
-        <div class="icon-button">
-          <fa-icon :icon="['fal', 'cog']" @click="showSettings" />
         </div>
-        <div class="icon-button">
-          <fa-icon :icon="['fal', lockIcon]" @click="changeLockSettings" />
+        <div v-else class="left-colum">
+          <div>
+            <account-tooltip type="Account" :account="account">
+              <div style="display: flex; flex-direction: row">
+                <div style="width: calc(100% - 45px)" @click="editName" class="flex-row flex-1">
+                  <div class="accountname ellipsis">{{ name }}</div>
+                  <fa-icon class="pen" :icon="['fal', 'fa-pen']" />
+                </div>
+                <div style=" width: 40px; text-align: center" @click="deleteAccount" class="trash flex-row ">
+                  <fa-icon :icon="['fal', 'fa-trash']" />
+                </div>
+              </div>
+              <div class="balance-row">
+                <span>{{ balanceForDisplay }}</span>
+                <span>{{ totalBalanceFiat }}</span>
+              </div>
+            </account-tooltip>
+          </div>
+        </div>
+        <div v-if="showBuySellButtons">
+          <button outlined class="small" @click="buyCoins" :disabled="buyDisabled">{{ $t("buttons.buy") }}</button>
+          <button outlined class="small" @click="sellCoins" :disabled="sellDisabled">{{ $t("buttons.sell") }}</button>
+        </div>
+        <div v-if="isSingleAccount" class="flex-row icon-buttons">
+          <div class="icon-button">
+            <fa-icon :icon="['fal', 'cog']" @click="showSettings" />
+          </div>
+          <div class="icon-button">
+            <fa-icon :icon="['fal', lockIcon]" @click="changeLockSettings" />
+          </div>
         </div>
       </div>
+      <input v-else ref="accountNameInput" type="text" v-model="newAccountName" @keydown="onKeydown" @blur="cancelEdit" />
     </div>
-    <input v-else ref="accountNameInput" type="text" v-model="newAccountName" @keydown="onKeydown" @blur="cancelEdit" />
   </div>
 </template>
 
@@ -44,16 +53,18 @@ import { AccountsController, BackendUtilities } from "../unity/Controllers";
 import { formatMoneyForDisplay } from "../util.js";
 import AccountTooltip from "./AccountTooltip.vue";
 import EventBus from "../EventBus";
+import ConfirmDialog from "./ConfirmDialog.vue";
 
 export default {
-  components: { AccountTooltip },
+  components: { AccountTooltip, ConfirmDialog },
   name: "AccountHeader",
   data() {
     return {
       editMode: false,
       newAccountName: null,
       buyDisabled: false,
-      sellDisabled: false
+      sellDisabled: false,
+      modal: null
     };
   },
   props: {
@@ -103,6 +114,9 @@ export default {
         this.$refs["accountNameInput"].focus();
       });
     },
+    deleteAccount() {
+      this.showConfirmModal();
+    },
     onKeydown(e) {
       switch (e.keyCode) {
         case 13:
@@ -146,6 +160,34 @@ export default {
     },
     changeLockSettings() {
       EventBus.$emit(this.unlocked ? "lock-wallet" : "unlock-wallet");
+    },
+    showConfirmModal() {
+      if (
+        this.account.allBalances.availableIncludingLocked === 0 &&
+        this.account.allBalances.unconfirmedIncludingLocked === 0 &&
+        this.account.allBalances.immatureIncludingLocked === 0
+      ) {
+        this.modal = { title: "Confirm Delete", message: "Are you sure you want to delete the account?", closeModal: this.closeModal, confirm: this.confirm };
+      } else {
+        this.modal = { title: "Error", message: "Your account needs to be empty before you can delete it", showButtons: false, closeModal: this.closeModal };
+      }
+    },
+    closeModal() {
+      this.modal = null;
+    },
+    confirm() {
+      this.$store.dispatch("app/SET_ACTIVITY_INDICATOR", true);
+      this.modal = null;
+      setTimeout(() => {
+        AccountsController.DeleteAccountAsync(this.account.UUID)
+          .then(() => {
+            this.$store.dispatch("app/SET_ACTIVITY_INDICATOR", false);
+          })
+          .catch(err => {
+            this.$store.dispatch("app/SET_ACTIVITY_INDICATOR", false);
+            alert(err.message);
+          });
+      }, 1000);
     }
   }
 };
@@ -203,8 +245,20 @@ button.small {
 .pen {
   display: none;
   position: absolute;
-  right: 5px;
+  right: 45px;
   line-height: 20px;
+}
+
+.trash {
+  display: none;
+  position: absolute;
+  right: 5px;
+  line-height: 18px;
+}
+
+.left-colum:hover .trash {
+  display: block;
+  color: #ff0000;
 }
 
 .left-colum:hover .pen {
---
+++ src/frontend/electron_vue/src/components/ConfirmDialog.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="modal-mask flex-col" @click="closeModal" v-if="showModal">
+    <div class="modal-container" @click.stop>
+      <div class="header">
+        <span :class="type">{{ title }}</span>
+        <div class="close" @click="closeModal">
+          <span class="icon">
+            <fa-icon :icon="['fal', 'times']" />
+          </span>
+        </div>
+      </div>
+      <div class="content">
+        {{ message }}
+      </div>
+      <app-button-section class="buttons" v-if="showButtons">
+        <template v-slot:right>
+          <button @click="closeModal">{{ $t("buttons.cancel") }}</button>
+          <button @click="confirm">{{ $t("buttons.confirm") }}</button>
+        </template>
+      </app-button-section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ConfirmDialog",
+  props: {
+    value: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    showModal() {
+      return this.value !== null;
+    },
+    title() {
+      return this.value.title || "Are you sure?";
+    },
+    showButtons() {
+      return typeof this.value.showButtons === "boolean" ? this.value.showButtons : true;
+    },
+    type() {
+      return this.value.type;
+    },
+    message() {
+      return this.value.message;
+    },
+    component() {
+      return this.value.component;
+    },
+    componentProps() {
+      return this.value.componentProps;
+    }
+  },
+  methods: {
+    closeModal() {
+      this.value.closeModal();
+    },
+    confirm() {
+      this.value.confirm();
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.modal-mask {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  margin-top: 0;
+  margin-left: 0;
+  align-items: center;
+  justify-content: center;
+  transition: opacity 0.3s ease;
+  z-index: 9998;
+}
+
+.modal-container {
+  max-width: 800px;
+  max-height: 700px;
+  width: calc(100% - 40px);
+  height: auto;
+  margin: 0px auto;
+  background-color: #fff;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+  transition: all 0.3s ease;
+}
+
+.header {
+  height: 56px;
+  line-height: 56px;
+  padding: 0 30px;
+  border-bottom: 1px solid var(--main-border-color);
+  font-weight: 600;
+  font-size: 1.05rem;
+
+  & .close {
+    float: right;
+    margin: 0 -10px 0 0;
+  }
+
+  & .icon {
+    line-height: 42px;
+    font-size: 1.2em;
+    font-weight: 300;
+    padding: 0 10px;
+    cursor: pointer;
+  }
+
+  & .icon:hover {
+    color: var(--primary-color);
+    background: var(--hover-color);
+  }
+}
+
+.content {
+  margin: 30px 0;
+  padding: 0 30px;
+  overflow-y: auto;
+}
+
+.buttons {
+  height: 64px;
+  padding: 0 12px;
+}
+
+.error {
+  color: var(--error-color, #dd3333);
+}
+</style>
