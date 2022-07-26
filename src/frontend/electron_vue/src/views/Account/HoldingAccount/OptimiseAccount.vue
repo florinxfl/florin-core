118f9514d9cfb99438a3d924c7ba52cd1d3724ca
Malcolm MacLeod <malcolm@gulden.com>
2022-07-26 19:47:13 +0200

    BUILD: Add missing file

---
+++ src/frontend/electron_vue/src/views/Account/HoldingAccount/OptimiseAccount.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="send-coins flex-col">
+    <portal to="sidebar-right-title">
+      {{ $t("buttons.optimise") }}
+    </portal>
+
+    <div class="main">
+      <app-form-field title="optimise_holding_account.funding_account">
+        <select-list :options="fundingAccounts" :default="fundingAccount" v-model="fundingAccount" />
+      </app-form-field>
+
+      <input
+        v-model="password"
+        type="password"
+        v-show="walletPassword === null"
+        :placeholder="$t('common.enter_your_password')"
+        :class="passwordClass"
+        @keydown="onPasswordKeydown"
+      />
+    </div>
+    <button @click="trySend" :disabled="disableSendButton">
+      {{ $t("buttons.send") }}
+    </button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+import { LibraryController, AccountsController, WitnessController } from "../../../unity/Controllers";
+import EventBus from "../../../EventBus";
+
+export default {
+  name: "OptimiseAccount",
+  data() {
+    return {
+      password: null,
+      fundingAccount: null,
+      isAmountInvalid: false,
+      isPasswordInvalid: false
+    };
+  },
+  computed: {
+    ...mapState("wallet", ["walletPassword"]),
+    ...mapGetters("wallet", ["accounts", "account"]),
+    computedPassword() {
+      return this.walletPassword ? this.walletPassword : this.password || "";
+    },
+    passwordClass() {
+      return this.isPasswordInvalid ? "error" : "";
+    },
+    fundingAccounts() {
+      return this.accounts.filter(x => x.state === "Normal" && ["Desktop"].indexOf(x.type) !== -1);
+    },
+    hasErrors() {
+      return this.isPasswordInvalid;
+    },
+    disableSendButton() {
+      if (this.computedPassword.trim().length === 0) return true;
+      return false;
+    }
+  },
+  mounted() {
+    if (this.fundingAccounts.length) {
+      this.fundingAccount = this.fundingAccounts[0];
+    }
+  },
+  methods: {
+    onPasswordKeydown() {
+      this.isPasswordInvalid = false;
+    },
+    trySend() {
+      /*
+       todo:
+        - improve notifications / messages on success and error
+       */
+
+      // wallet needs to be unlocked to make a payment
+      if (LibraryController.UnlockWallet(this.computedPassword, 120) === false) {
+        this.isPasswordInvalid = true;
+      }
+
+      if (this.hasErrors) return;
+
+      let accountUUID = AccountsController.GetActiveAccount();
+      let optimalAmounts = WitnessController.GetOptimalWitnessDistributionForAccount(accountUUID);
+      let result = WitnessController.OptimiseWitnessAccount(this.account.UUID, this.account.UUID, optimalAmounts);
+
+      if (result.result === true) {
+        this.password = null;
+        this.fundingAccount = null;
+
+        EventBus.$emit("close-right-sidebar");
+      } else {
+        alert(result.info);
+      }
+
+      // lock the wallet again
+      LibraryController.LockWallet();
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.send-coins {
+  height: 100%;
+
+  .main {
+    flex: 1;
+  }
+}
+
+button {
+  width: 100%;
+}
+</style>
