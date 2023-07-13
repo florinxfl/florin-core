f782be42278defe83b121dcc5649ea7b69ea55fd
Malcolm MacLeod <malcolm@gulden.com>
2023-07-13 23:35:01 +0200

    REPO: Update licensing
    Change license to GNU Lesser General Public License v3

--- src/unity/ios/gulden_static/source/dummy.cpp
+++ src/unity/ios/gulden_static/source/dummy.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2018-2022 The Centure developers
 // Authored by: Willem de Jonge (willem@isnapp.nl)
-// Distributed under the Libre Chain License, see the accompanying
+// Distributed under the GNU Lesser General Public License v3, see the accompanying
 // file COPYING
 
 // This is a dummy C++ file which is only present to trigger Xcode to link the required runtime
